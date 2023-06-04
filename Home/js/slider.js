ba0fee56edbeee4b80924e8049d7e1f59441c37f
Jean Carlos Severino <jeancarlos.seven@gmail.com>
2023-06-04 18:31:45 -0300

    new

--- Home/js/slider.js
+++ Home/js/slider.js
@@ -1,15 +1,71 @@
-let count = 1
-document.getElementById("radio1").checked = true;
+var carousel = document.getElementById('carousel');
+var slides = 3;
+var speed = 7000; // 5 seconds
 
-setInterval( function(){
+function carouselHide(num) {
+    indicators[num].setAttribute('data-state', '');
+    slides[num].setAttribute('data-state', '');
 
-}, 5000)
+    slides[num].style.opacity=0;
+}
 
-function nextImage(){
-    count++;
-    if(count>4){
-     count = 1;
+function carouselShow(num) {
+    indicators[num].checked = true;
+    indicators[num].setAttribute('data-state', 'active');
+    slides[num].setAttribute('data-state', 'active');
+
+    slides[num].style.opacity=1;
+}
+
+function setSlide(slide) {
+    return function() {
+        // Reset all slides
+        for (var i = 0; i < indicators.length; i++) {
+            indicators[i].setAttribute('data-state', '');
+            slides[i].setAttribute('data-state', '');
+            
+            carouselHide(i);
+        }
+
+        // Set defined slide as active
+        indicators[slide].setAttribute('data-state', 'active');
+        slides[slide].setAttribute('data-state', 'active');
+        carouselShow(slide);
+
+        // Stop the auto-switcher
+        clearInterval(switcher);
+    };
+}
+
+function switchSlide() {
+    var nextSlide = 0;
+
+    // Reset all slides
+    for (var i = 0; i < indicators.length; i++) {
+        // If current slide is active & NOT equal to last slide then increment nextSlide
+        if ((indicators[i].getAttribute('data-state') == 'active') && (i !== (indicators.length-1))) {
+            nextSlide = i + 1;
+        }
+
+        // Remove all active states & hide
+        carouselHide(i);
     }
 
-    document.getElementById("radio"+count).checked = true;
+    // Set next slide as active & show the next slide
+    carouselShow(nextSlide);
+}
+
+if (carousel) {
+    var slides = carousel.querySelectorAll('.slide');
+    var indicators = carousel.querySelectorAll('.indicator');
+
+    var switcher = setInterval(function() {
+        switchSlide();
+    }, speed);
+
+    for (var i = 0; i < indicators.length; i++) {
+        indicators[i].addEventListener("click", setSlide(i));
+    }
+
+    carouselShow(0);
 }
